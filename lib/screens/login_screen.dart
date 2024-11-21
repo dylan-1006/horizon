66c089881b359aa1cb0a838817d1863af183c93a
Dylan <eu.dylan@gmail.com>
2024-11-21 17:29:10 +0000

    Added login & register icons

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,18 +9,26 @@ class LoginScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          Container(
+              margin: EdgeInsets.only(top: 70),
+              child: Center(
+                child: Image.asset('assets/icons/login_icon.png'),
+              )),
           Center(
             child: Container(
+              padding: EdgeInsets.only(top: 0),
+              color: Colors.amber,
               child: Text(
                 "Welcome back!",
                 style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'Garamond',
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold),
+                  color: Colors.black,
+                  fontFamily: 'Garamond',
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
