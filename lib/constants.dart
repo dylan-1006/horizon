724258e5181a4e026d3e51f29a0813ef82ba6a96
Dylan <eu.dylan@gmail.com>
2024-11-22 17:43:56 +0000

    Added new font variations for Open Sans and coded basic ui for login screen.

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const Color primaryColor = Color(0xff3048EA);
+  static const Color secondaryColor = Color(0xffF7E8E7);
+  static Color accentColor = Colors.black.withOpacity(0.65);
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:horizon/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,29 +7,139 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-              margin: EdgeInsets.only(top: 70),
+              margin: EdgeInsets.only(top: 80),
               child: Center(
-                child: Image.asset('assets/icons/login_icon.png'),
+                child: Image.asset(
+                  'assets/icons/login_icon.png',
+                  width: 160,
+                ),
               )),
           Center(
-            child: Container(
-              padding: EdgeInsets.only(top: 0),
-              color: Colors.amber,
-              child: Text(
-                "Welcome back!",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Garamond',
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                ),
+            child: Text(
+              "Welcome back!",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Garamond',
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
+          Container(
+            width: double.infinity,
+            height: 55,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(12)),
+            margin: const EdgeInsets.only(left: 35, right: 35, top: 30),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            alignment: Alignment.centerLeft,
+            child: Form(
+                child: Text(
+              "Email",
+              style: TextStyle(
+                  fontFamily: 'Open Sans',
+                  fontSize: 15,
+                  color: Constants.accentColor),
+            )),
+          ),
+          Container(
+            width: double.infinity,
+            height: 55,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(12)),
+            margin: const EdgeInsets.only(left: 35, right: 35, top: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            alignment: Alignment.centerLeft,
+            child: Form(
+                child: Text(
+              "Password",
+              style: TextStyle(
+                  fontFamily: 'Open Sans',
+                  fontSize: 15,
+                  color: Constants.accentColor),
+            )),
+          ),
+          Container(
+              margin: const EdgeInsets.only(right: 35, top: 8),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Forgot your password?",
+                    style: TextStyle(
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 12,
+                        color: Constants.accentColor),
+                  ))),
+          Container(
+            width: double.infinity,
+            height: 55,
+            decoration: BoxDecoration(
+                color: Constants.primaryColor,
+                borderRadius: BorderRadius.circular(12)),
+            margin: const EdgeInsets.only(left: 35, right: 35, top: 30),
+            alignment: Alignment.centerLeft,
+            child: Form(
+                child: Center(
+              child: const Text(
+                "Log In",
+                style: TextStyle(
+                    fontFamily: 'Open Sans',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
+            )),
+          ),
+          Container(
+            width: double.infinity,
+            height: 55,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(12)),
+            margin: const EdgeInsets.only(left: 35, right: 35, top: 70),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            alignment: Alignment.centerLeft,
+            child: Form(
+                child: Text(
+              "Sign in with Google",
+              style: TextStyle(
+                  fontFamily: 'Open Sans',
+                  fontSize: 15,
+                  color: Constants.accentColor),
+            )),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(vertical: 60),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Dont have an account? ",
+                  style: TextStyle(
+                      fontFamily: 'Open Sans',
+                      fontSize: 15,
+                      color: Colors.black),
+                ),
+                Container(
+                  child: Text("Sign up",
+                      style: TextStyle(
+                          fontFamily: 'Open Sans',
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Constants.primaryColor)),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
