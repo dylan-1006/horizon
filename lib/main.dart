8c32b8c0e36198600bc2b415a8d7e1773a4d3ed7
Dylan <eu.dylan@gmail.com>
2025-01-05 23:50:12 +0000

    Set to initialise firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:horizon/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
