8abf2a49293141a7212bf08da8374d3cf91b1cb6
Dylan <eu.dylan@gmail.com>
2025-03-04 04:15:27 +0000

    Added pull down to refresh for homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,74 +15,94 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Map<String, dynamic> userData = {};
+  late Future<void> _fetchUserDataFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserDataFuture = fetchUserData();
+  }
+
   Future<void> fetchUserData() async {
-    String userId = await Auth().getUserId();
-    userData = await DatabaseUtils.getUserData(userId);
+    try {
+      String userId = await Auth().getUserId();
+      var data = await DatabaseUtils.getUserData(userId);
+      setState(() {
+        userData = data;
+      });
+    } catch (e) {
+      debugPrint('Error fetching user data: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: fetchUserData(),
+      future: _fetchUserDataFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const LoadingScreen();
         } else if (snapshot.hasError) {
-          return ErrorScreen(onRefresh: () {});
+          return ErrorScreen(onRefresh: fetchUserData);
         } else {
           return Scaffold(
             backgroundColor: const Color(0xfff2f2f7),
             extendBodyBehindAppBar: true,
-            body: SingleChildScrollView(
-              child: Container(
-                height: 1100,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage(
-                            'assets/images/home_screen_background.png'),
-                        fit: BoxFit.cover)),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          width: 55,
-                          height: 55,
-                          margin: const EdgeInsets.only(left: 20, top: 85),
-                          decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.contain,
-                                  image:
-                                      AssetImage('assets/icons/app_icon.png'))),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            NavigationUtils.push(
-                                context, SettingsProfileScreen());
-                          },
-                          child: Container(
-                            width: 45,
-                            height: 45,
-                            margin: const EdgeInsets.only(right: 20, top: 95),
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
+            body: RefreshIndicator(
+              onRefresh: fetchUserData,
+              child: SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child: Container(
+                  height: 1100,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/home_screen_background.png'),
+                          fit: BoxFit.cover)),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 55,
+                            height: 55,
+                            margin: const EdgeInsets.only(left: 20, top: 85),
+                            decoration: const BoxDecoration(
                                 image: DecorationImage(
                                     fit: BoxFit.contain,
-                                    image: userData['profileImgUrl'] != null
-                                        ? NetworkImage(
-                                            userData['profileImgUrl'])
-                                        : const AssetImage(
-                                            'assets/images/default_user_profile_picture.jpg'))),
+                                    image: AssetImage(
+                                        'assets/icons/app_icon.png'))),
                           ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      child: const Text("daily reflection."),
-                    )
-                  ],
+                          GestureDetector(
+                            onTap: () {
+                              NavigationUtils.push(
+                                  context, SettingsProfileScreen());
+                            },
+                            child: Container(
+                              width: 45,
+                              height: 45,
+                              margin: const EdgeInsets.only(right: 20, top: 95),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  fit: BoxFit.contain,
+                                  image: userData['profileImgUrl'] != null
+                                      ? NetworkImage(userData['profileImgUrl'])
+                                      : const AssetImage(
+                                          'assets/images/default_user_profile_picture.jpg'),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: const Text("daily reflection."),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
