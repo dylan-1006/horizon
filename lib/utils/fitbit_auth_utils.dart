9b9e1a02ba06875f4168e9d8fa7905205c1f370a
Dylan <eu.dylan@gmail.com>
2025-03-04 03:12:31 +0000

    Fixed bug where fitbit token refresh function would not work as intended.

--- lib/utils/fitbit_auth_utils.dart
+++ lib/utils/fitbit_auth_utils.dart
@@ -8,6 +8,24 @@ class FitbitAuthUtils {
   static const String clientSecret = "1190125792b923fdf6d03f2dc53d5c6c";
   static const String tokenUrl = "https://api.fitbit.com/oauth2/token";
 
+  static Future<DateTime> getFirebaseTime() async {
+    try {
+      DocumentReference docRef = await FirebaseFirestore.instance
+          .collection('timestamps')
+          .add({'timestamp': FieldValue.serverTimestamp()});
+
+      DocumentSnapshot snapshot = await docRef.get();
+      Timestamp timestamp = snapshot.get('timestamp');
+
+      await docRef.delete();
+
+      return timestamp.toDate();
+    } catch (e) {
+      print('Error getting Firebase server time: $e');
+      return DateTime.now();
+    }
+  }
+
   static Future<void> refreshAccessToken(String userId) async {
     try {
       Map<String, dynamic> userData = await DatabaseUtils.getUserData(userId);
@@ -19,9 +37,9 @@ class FitbitAuthUtils {
       DateTime expiryTime = userData.containsKey("fitbitTokenExpiry")
           ? (userData["fitbitTokenExpiry"].toDate().toUtc())
           : updatedTime;
+      DateTime currentTime = await getFirebaseTime();
 
-
-      if (updatedTime.isBefore(expiryTime)) {
+      if (currentTime.isBefore(expiryTime)) {
         print("Token is still valid. No need to refresh.");
         print(DateTime.now());
         print(expiryTime);
